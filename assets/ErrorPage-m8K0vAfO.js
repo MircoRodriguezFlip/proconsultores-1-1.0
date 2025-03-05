05779d514954e92dbc4b575ab891194ef38d8ecd
mircomania <mirco.rodriguez@hotmail.com>
2025-03-05 19:28:33 -0300

    Updates

---
+++ assets/ErrorPage-m8K0vAfO.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DNQPV0eV.js";const o=()=>r.jsx("main",{children:r.jsx("h1",{children:"Error Page"})});export{o as default};
---
+++ assets/PoliticasPage-DOwpCqwb.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index-DNQPV0eV.js";const t=()=>i.jsx("main",{children:i.jsx("h1",{children:"PoliticasPage"})});export{t as default};
