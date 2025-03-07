97cc055cc359020d999547faac28e7da0257bdfb
mircomania <mirco.rodriguez@hotmail.com>
2025-03-07 20:56:41 -0300

    Updates

---
+++ assets/ErrorPage-sXlVmzK8.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BJ_Yy5bj.js";const o=()=>r.jsx("main",{children:r.jsx("h1",{children:"Error Page"})});export{o as default};
---
+++ assets/PoliticasPage-CHKV_g7f.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index-BJ_Yy5bj.js";const t=()=>i.jsx("main",{children:i.jsx("h1",{children:"PoliticasPage"})});export{t as default};
